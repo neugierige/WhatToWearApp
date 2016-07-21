31b7afce57e9ba46469f40454c0f436b23e5b2f3
Luyuan Xing <lxing.nyc@gmail.com>
2016-07-21 11:47:15 -0400

    added table to 2nd view

--- WhatToWearApp/AddItemViewController.swift
+++ WhatToWearApp/AddItemViewController.swift
@@ -8,19 +8,43 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
+class AddItemViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     let mainVC = MainViewController()
+    let clothing = Clothing()
     let table = UITableView()
     
-    override func viewWillAppear(_ animated: Bool) {
-
+    override func viewWillLayoutSubviews() {
+        self.view.backgroundColor = UIColor.white()
+        self.view.addSubview(table)
+        self.table.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addConstraint(NSLayoutConstraint(item: table, attribute: .centerX, relatedBy: .equal, toItem: self.view, attribute: .centerX, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: table, attribute: .width, relatedBy: .equal, toItem: self.view, attribute: .width, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: table, attribute: .top, relatedBy: .equal, toItem: self.topLayoutGuide, attribute: NSLayoutAttribute.bottom, multiplier: 1.0, constant: 0.0))
+        self.view.addConstraint(NSLayoutConstraint(item: table, attribute: .bottom, relatedBy: .equal, toItem: self.view, attribute: .bottom, multiplier: 1.0, constant: 0.0))
     }
     
     override func viewDidLoad() {
-        self.view.backgroundColor = UIColor.red()
-        mainVC.navigationController?.setToolbarHidden(false, animated: false)
+        
     }
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return clothing.items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as UITableViewCell
+        return cell
+    }
+    
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        <#code#>
+//    }
     
     
 }
